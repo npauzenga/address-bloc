8aa319097747ee8671ce1cbdcdd4a63760d785ef
Nate Pauzenga <npauzenga@gmail.com>
2015-09-21 18:44:35 -0400

    fixed remove_entry bug and refactored

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,16 +21,12 @@ class AddressBook
   end
 
   def remove_entry(name, phone, email)
-    # using index where I could have set a delete_entry variable
-    index = 0
 
     @entries.each do |entry|
-      if name == entry.name && phone = entry.phone_number && email == entry.email
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+        @entries.delete(entry)
         break
       end
-      index += 1
     end
-    # could have said @entries.delete(delete_entry)
-    @entries.delete_at(index)
   end
 end
