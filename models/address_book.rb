fa8dd5ef5202c49d916da0d30abcab24a8bdb271
Nate Pauzenga <npauzenga@gmail.com>
2015-09-23 20:14:14 -0400

    Change all single quotes to double

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
-require_relative 'entry.rb'
-require 'csv'
+require_relative "entry.rb"
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -33,7 +33,7 @@ class AddressBook
 
     csv.each do |row|
       row_hash = row.to_hash
-      add_entry(row_hash['name'], row_hash['phone_number'], row_hash['email'])
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
 
